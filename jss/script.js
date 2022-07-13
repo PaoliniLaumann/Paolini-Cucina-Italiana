48c23510d8db2eefc41c214208d05fdac148a2ce
German Paolini Laumann <paolinilaumann@gmail.com>
2022-07-13 20:44:23 -0300

    Se arregla json con array de carrito, se agrega fetch para traer array

--- jss/script.js
+++ jss/script.js
@@ -22,7 +22,7 @@ class Producto {
 }
 
 const imprimirProductosEnHTML = () => {
-  fetch(`/../json/productos.json`)
+  fetch(`../../json/productos.json`)
     .then((res) => res.json())
     .then((array) => {
       console.log(array);
